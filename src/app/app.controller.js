0c915ffe662d810e99bb6d548fca2229aed00444
ginesortiz <gines.ortiz@billymob.com>
2016-10-22 08:54:40 +0200

    Loader module created and attached to ui router events

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,7 +1,44 @@
 class appController {
-  constructor() {
+  constructor($scope, LoaderService) {
+    this.LoaderService = LoaderService;
 
+    const stateStart = (event, toState, toParams, fromState) => {
+      'use strict';
+
+      if (!fromState.abstract) {
+        this.LoaderService.loaderStatus({
+          status: true,
+          info: {
+            text: 'Loading...'
+          }
+        });
+      }
+    };
+
+    const stateError = (event) => {
+      event.preventDefault();
+
+      this.LoaderService.loaderStatus({status: false});
+    };
+
+    const stateSuccess = (event, toState, toParams, fromState) => {
+      'use strict';
+
+      this.LoaderService.loaderStatus({
+        status: false
+      });
+
+      if (fromState.abstract) {
+        stateStart(event, toState, toParams, fromState);
+      }
+    };
+
+    $scope.$on('$stateChangeStart', stateStart);
+    $scope.$on('$stateChangeSuccess', stateSuccess);
+    $scope.$on('$stateChangeError', stateError);
   }
 }
+
+appController.$inject = ['$scope', 'LoaderService'];
 
 export {appController};
