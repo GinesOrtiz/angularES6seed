b9a1a0934050261c16ebe7753e53cb904632a79f
ginesortiz <gines.ortiz@billymob.com>
2016-10-22 07:52:52 +0200

    Add otherwise config

--- src/app/index.js
+++ src/app/index.js
@@ -6,10 +6,18 @@ import ngSanitize from 'angular-sanitize';
 import 'normalize.css/normalize.css';
 import './shared/general.scss';
 
+const appConfig = ($urlRouterProvider) => {
+  'use strict';
+  $urlRouterProvider.otherwise('/');
+};
+
+appConfig.$inject = ['$urlRouterProvider'];
+
 const app = angular.module('app', [
     uiRouter,
     ngSanitize,
     ngstorage.name
-]);
+])
+  .config(appConfig);
 
 export {app};
